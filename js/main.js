b39544557b31fc2af563bf9564fbf6af1785b028
harrison mweene <harrisonmweene11@gmail.com>
2020-09-25 21:49:42 +0200

    Backing up code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
     $('.header').height($(window).height());
 })
+
+//page pafination
+
+$('li').click(function() {
+  $(this).addClass('active').siblings().removeClass('active');
+});
