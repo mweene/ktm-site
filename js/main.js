f95368991517e8ab7b88b9a7dc22cb15d13bcaf5
harrison mweene <harrisonmweene11@gmail.com>
2020-10-11 00:29:53 +0200

    Add the offiicial logo in the navbar

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('.header').height($(window).height());
 })
 
-//page pafination
+//page pagination
 
 $('li').click(function() {
   $(this).addClass('active').siblings().removeClass('active');
