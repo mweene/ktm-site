34674762e84ce16cb2daf6fd91c9ec2330335b46
mweene <harrisonmweene11@gmail.com>
2026-02-22 17:55:30 -0800

    Add React to the website

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.header').height($(window).height());
-})
-
-//page pagination
-
-$('li').click(function() {
-  $(this).addClass('active').siblings().removeClass('active');
-});
