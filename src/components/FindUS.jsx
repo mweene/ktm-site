5ad271e660f8e7c7506ee9ae6062569492141f37
mweene <harrisonmweene11@gmail.com>
2026-03-18 13:54:54 +0200

    Add Links to the Footer component

--- src/components/FindUS.jsx
+++ src/components/FindUS.jsx
@@ -1,7 +1,47 @@
+import Pin from "../assets/MaterialSymbolsLocationOnRounded.svg";
+
 export default function FindUS() {
-  return (
-    <div className="">
-      <h2 className="font-bold text-7xl">find us component</h2>
-    </div>
-  )
+    const services = [
+        { id: 1, name: "sabbath school", time: "09:00" },
+        { id: 2, name: "classes", time: "10:00" },
+        { id: 3, name: "main service", time: "11:00" },
+        { id: 4, name: "lunch", time: "13:00" },
+        { id: 5, name: "bible study", time: "15:00" },
+    ];
+    return (
+        <section className="grid place-content-center">
+            <div className="grid grid-cols-2 place-items-center bg-green-200 w-fit rounded-3xl">
+                <div className="bg-neutral-300 p-15 rounded-l-3xl">
+                    <div className="header-text">
+                        <p className="uppercase">find us</p>
+                        <h2 className="text-5xl font-semibold">
+                            Come worship with us
+                        </h2>
+
+                        <ul className="grid gap-6 mt-7.5">
+                            {services.map((service) => (
+                                <li
+                                    key={service.id}
+                                    className="flex place-content-between border-b border-neutral-500 pb-2.5"
+                                >
+                                    <span className="capitalize text-sm">
+                                        {service.name}
+                                    </span>
+                                    <span className="font-medium">{service.time} Hrs</span>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="text-center grid place-items-center">
+                    <img src={Pin} alt="pin logo" className="h-[4rem]" />
+                    <p className="font-medium">Katimamulilo SDA Church</p>
+                    <p className="font-medium">Lusaka, Zambia</p>
+
+                    <button className="uppercase border py-2 px-5">get directions</button>
+                </div>
+            </div>
+        </section>
+    );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,52 @@
-import Logo from "../assets/footerLogo.png"
+import { Link } from "react-router-dom";
+import Logo from "../assets/footerLogo.png";
+
 export default function Footer() {
   return (
-    <div className="p-4 bg-neutral-300 grid grid-cols-3 gap-4 md:mt-30">
-		<img src={Logo} alt="logo" className="h-[20rem]" />
-		<div className="p-4 bg-pink-300"></div>
-		<div className="p-4 bg-pink-100"></div>
+    <section>
+    <div className="px-4 py-10 bg-neutral-300 grid md:grid-cols-4 gap-4 md:mt-30">
+      <div>
+		    <img src={Logo} alt="logo" className="h-[14rem]" />
+      </div>
+
+		  <div className="p-4">
+        <h3 className="uppercase font-semibold mb-2">media</h3>
+      
+        <ul className="grid gap-2">
+          <li><Link to="/sermons">sermons</Link></li>
+          <li><Link to="/gallery">gallery</Link></li>
+        </ul>
+      </div>
+      
+      <div className="p-4">
+        <h3 className="uppercase font-semibold mb-2">socials</h3>
+      
+        <ul className="grid gap-2">
+          <li>facebook</li>
+          <li>instagram</li>
+          <li>youtube</li>
+          <li>whatsapp</li>
+          <li>email</li>
+        </ul>
+      </div>
+
+        <div className="p-4">
+          <h3 className="uppercase font-semibold mb-2">church</h3>
+      
+          <ul className="grid gap-2">
+            <li><Link to="/">home</Link></li>
+            <li><Link to="/sermons">sermons</Link></li>
+            <li><Link to="/events">events</Link></li>
+            <li><Link to="/about">about us</Link></li>
+            <li><Link to="/contact">contact</Link></li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="bg-neutral-700 text-neutral-300 px-7.5 py-4 flex place-content-between">
+        <p className="">ktm sda church. lusaka, zambia</p>
+        <p>Copyright (c) 2026 Author. All Rights Reserved.</p>      
     </div>
+    </section>
   )
 }
