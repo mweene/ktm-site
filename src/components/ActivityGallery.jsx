faae72f76eee793626faf5fe9dfee094bfd4e048
mweene <harrisonmweene11@gmail.com>
2026-03-23 15:47:15 +0200

    Filter by activity now works in the ActivityGallery component

--- src/components/ActivityGallery.jsx
+++ src/components/ActivityGallery.jsx
@@ -11,8 +11,8 @@ const data = [
       { "id": "a1-01", "type": "video", "src": "https://lorem.video/720p_h264_10s", "span": "col-span-3" },
       { "id": "a1-02", "type": "image", "src": "https://picsum.photos/id/221/600/400?grayscale", "span": "col-span-2" },
       { "id": "a1-03", "type": "text",  "content": "Fun", "span": "col-span-1", "bgColor": "bg-blue-500" },
-      { "id": "a1-04", "type": "image", "src": "https://picsum.photos/id/222/600/400?grayscale", "span": "col-span-2" },
-      { "id": "a1-05", "type": "image", "src": "https://picsum.photos/id/223/600/400?grayscale", "span": "col-span-1" },
+      { "id": "a1-04", "type": "text", "content": "more fun, everyday", "span": "col-span-2", "bgColor": "bg-neutral-300"},
+      { "id": "a1-05", "type": "video", "src": "https://lorem.video/cat_128kbps", "span": "col-span-1" },
       { "id": "a1-06", "type": "images", "src": "https://picsum.photos/id/122/600/400?grayscale", "span": "col-span-4" },
     ]
   },
@@ -27,6 +27,7 @@ const data = [
       { "id": "a2-03", "type": "text",  "content": "Joy", "span": "col-span-1", "bgColor": "bg-green-500" },
       { "id": "a2-04", "type": "image", "src": "https://picsum.photos/id/322/600/400?grayscale", "span": "col-span-2" },
       { "id": "a2-05", "type": "image", "src": "https://picsum.photos/id/323/600/400?grayscale", "span": "col-span-1" },
+      { "id": "a1-06", "type": "images", "src": "https://picsum.photos/id/324/600/400?grayscale", "span": "col-span-4" },      
     ]
   },
   {
@@ -38,8 +39,9 @@ const data = [
       { "id": "a3-01", "type": "video", "src": "https://lorem.video/720p_h264_15s", "span": "col-span-3" },
       { "id": "a3-02", "type": "image", "src": "https://picsum.photos/id/421/600/400?grayscale", "span": "col-span-2" },
       { "id": "a3-03", "type": "text",  "content": "Love", "span": "col-span-1", "bgColor": "bg-red-500" },
-      { "id": "a3-04", "type": "image", "src": "https://picsum.photos/id/422/600/400?grayscale", "span": "col-span-2" },
-      { "id": "a3-05", "type": "image", "src": "https://picsum.photos/id/423/600/400?grayscale", "span": "col-span-1" },
+      { "id": "a3-04", "type": "image", "src": "https://picsum.photos/id/427/600/400?grayscale", "span": "col-span-2" },
+      { "id": "a3-05", "type": "image", "src": "https://picsum.photos/id/426/600/400?grayscale", "span": "col-span-1" },
+      { "id": "a1-06", "type": "images", "src": "https://picsum.photos/id/424/600/400?grayscale", "span": "col-span-4" },      
     ]
   }
 ];
@@ -49,14 +51,29 @@ function EventCard({className, type, src, content}) {
 
   const imagesList = [
     'https://picsum.photos/id/600/600/400?grayscale',
-    'https://picsum.photos/id/601/600/400?grayscale',
+    'https://picsum.photos/id/605/600/400?grayscale',
     'https://picsum.photos/id/602/600/400?grayscale'
   ];
 
+  const handlePrev = () => setIndex(prev => prev - 1);
+  const handleNext = () => setIndex(prev => prev + 1);
+
+  const styles = `
+    rounded-4xl overflow-hidden relative h-[20rem] ${className}
+    bg-neutral-300
+  `;
+
   return (
-    <div className={`rounded-4xl overflow-hidden relative h-[21rem] ${className}`}>
+    <div className={styles}>
       {type === 'image' && (
-        <img src={src} alt="some random image" className="w-full h-full object-cover rounded-4xl"/>
+        <img 
+          src={src} 
+          alt="some random image" 
+          className={`
+            w-full h-full object-cover rounded-xl transition
+            duration-300 ease-in-out hover:scale-130
+          `}
+        />
       )}
 
       {type === 'images' && (
@@ -64,8 +81,17 @@ function EventCard({className, type, src, content}) {
           <img src={imagesList[index]} alt="some image" className="w-full h-full object-cover rounded-4xl" />
           
           <div className="absolute bottom-0 right-0 m-6.5 flex gap-4">
-            <button className="rounded-full p-2 bg-neutral-100/90"><ArrowLeft size={20}/></button>
-            <button className="rounded-full p-2 bg-neutral-100/90"><ArrowRight size={20}/></button>
+            <button onClick={handlePrev} 
+              className="rounded-full p-2 bg-neutral-100/90 transition duration-300 ease-in-out hover:scale-110"
+            >
+              <ArrowLeft size={20}/>
+            </button>
+
+            <button onClick={handleNext} 
+              className="rounded-full p-2 bg-neutral-100/90 transition duration-300 ease-in-out hover:scale-110"
+            >
+              <ArrowRight size={20}/>
+            </button>
           </div>
         </>
       )}
@@ -75,7 +101,7 @@ function EventCard({className, type, src, content}) {
       )}
 
       {type === 'text' && (
-        <div className="w-full h-full bg-blue-500 flex place-content-center place-items-center text-white text-9xl font-bold rounded-4xl">
+        <div className="w-full h-full bg-gradient-to-tl from-neutral-400 to-neutral-700 text-blue-50 flex place-content-center place-items-center text-5xl font-bold rounded-4xl">
           {content}
         </div>
       )}
@@ -84,7 +110,14 @@ function EventCard({className, type, src, content}) {
 }
 
 export default function ActivityGallery () {
-    return (
+  const [activity, setActivity] = useState(data[0]);
+  
+  const handleActivity = (event) => {
+    const newActivity = data.find(d => d.activityName === event.target.value);
+    setActivity(newActivity);
+  }
+
+  return (
       <>
         <div>
           <p className="uppercase">our gallery</p>
@@ -93,11 +126,20 @@ export default function ActivityGallery () {
           </h2>
 
           <div className="flex gap-4 my-4 mt-6">
-            {data.map(d => <p key={d.id}>{d.activityName}</p>)}
+            {data.map(d => (
+              <button 
+                key={d.id} 
+                value={d.activityName}
+                className="text-sm"
+                onClick={() => handleActivity(event)}
+              >
+                  {d.activityName}
+              </button>
+            ))}
           </div>
 
           <div className="grid gap-4 grid-cols-7">
-            {data[0].grid.map(item => (
+            {activity.grid.map(item => (
               <EventCard 
                 key={item.id}
                 className={item.span}
