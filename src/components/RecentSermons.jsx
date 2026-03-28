545031d7c0b7dfa3f3c45a2d31e1c188fd099762
mweene <harrisonmweene11@gmail.com>
2026-03-28 23:06:36 +0200

    Navigate to sermons page when recent sermons card is clicked

--- src/components/RecentSermons.jsx
+++ src/components/RecentSermons.jsx
@@ -1,11 +1,45 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { ArrowRight } from "lucide-react";
 import PlayLogo from "../assets/icons/play.svg";
 import VideoPlayer from "./VideoPlayer";
 import vid from "../assets/demo-bg.mp4";
 
+const data = {
+  video: {
+    id: 'h-332e',
+    src: `${vid}`,
+    height: "",
+    width: ""
+  },
+  cards: [
+    {
+      id: '9-212f',
+      preacher: "dc j chanza",
+      title: "the coming of christ",
+      date: "2026-03-28",
+      program: "bible study",
+    },
+    {
+      id: '1-052r',
+      preacher: "eld c chongo",
+      title: "the 10 virgins",
+      date: "2026-03-28",
+      program: "main service",
+    },
+  ]
+}
+
 export default function RecentSermons() {
     const [isPlaying, setIsPlaying] = useState(false);
+    const navigate = useNavigate();
+
+    const handleCardClick = (event) => {
+      const vidId = event.target.parentNode.value ?? '';
+      
+      navigate(`/sermons/id?=${vid}`);
+      console.log(event.target.parentNode.value);
+    }
 
     return (
         <section className="md:mt-30">
@@ -20,48 +54,58 @@ export default function RecentSermons() {
                 
                 <a href="/sermons" className="self-end flex capitalize items-center gap-2">
                     more sermons 
-                    <ArrowRight size={20} />
                 </a>
             </div>
             <div className="cards grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
               <div className="lg:col-span-2 min-h-92">
-                <VideoPlayer src={vid} />  
+                <VideoPlayer id={data.video.id} src={data.video.src} />  
               </div>
               
-              <Card />
-              <Card />
+              {data.cards.map(card => (
+                <Card 
+                  key={card.id}
+                  preacher={card.preacher}
+                  title={card.title}
+                  date={card.date}
+                  program={card.program}
+                  value={`${card.id}`}
+                  onClick={(event) => handleCardClick(event)}
+                />
+              ))}
             </div>
         </section>
     );
 }
 
-function Card({ onClick }) {
+function Card({ onClick, preacher, title, date, program, value }) {
     return (
         <div className="card rounded-4xl min-h-92 bg-neutral-300">
           <div className="card-content p-6 grid place-content-between h-full">
             <p className="border border-neutral-400 bg-neutral-200 py-1 px-4 rounded-full text-sm w-fit">
-              eld m. milanzi
+              {preacher}
             </p>
             <div>
-              <h3 className="text-4xl leading-none">By his words we are saved</h3>
+              <h3 className="text-4xl leading-none">{title}</h3>
 
               <div className={`
                 flex gap-4 [&>p]:text-[.79rem] mt-2 uppercase
                 font-medium [&>p]:text-neutral-700
               `}>
-                <p>21 march 2026</p>
-                <p>week of prayer</p> 
+                <p>{date}</p>
+                <p>{program}</p> 
               </div>
                     
               <button
                 onClick={onClick}
                 className={`
                   flex items-center gap-2 mt-10 capitalize w-fit rounded-full
-                  border border-neutral-900 pl-4 pr-2 py-1
-                `}>
+                  text-neutral-50 bg-neutral-900 pl-4 pr-2 py-1
+                `}
+                value={value}
+              >
 
-                <span>watch sermon</span>
-                <img src={PlayLogo} alt="play logo" className="h-[2.5rem] bg-neutral-50 p-2 rounded-full"/>
+                <span className="text-[1rem]">watch sermon</span>
+                <img src={PlayLogo} alt="play logo" className="h-[2.2rem] bg-neutral-50 p-1.5 rounded-full"/>
               </button>
             </div>
           </div>
